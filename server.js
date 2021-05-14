e1c38adeb809f93ba669de67de26d39dd393e0b3
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-05-13 22:02:48 -0400

    added res.sendfile code snippet for heroku

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const port = process.env.PORT || 5000
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("frontend/build"));
 
-app.listen(port, () => console.log(`Server is listening at port ${port}`))
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is listening at port ${port}`));
