903906a48f22425bad154ef809f1b98302da81eb
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-05-14 08:57:47 -0400

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 
+app.use(cors());
+
 app.get("/", (req, res) => res.send("Hello world"))
 
 app.get("/data", (req, res) => res.json({ data: "Jacob" }))
