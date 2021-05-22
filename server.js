2b410e81570a55707cce81b093b578758a8975d7
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-05-22 18:22:52 -0400

    took client code snippet out of server

--- server.js
+++ server.js
@@ -39,16 +39,16 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   // Exprees will serve up production assets
+//   app.use(express.static("client/build"));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   // Express serve up index.html file if it doesn't recognize route
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use('/users', userRouter)
 
