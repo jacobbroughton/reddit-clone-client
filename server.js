f7f249ea01a952f0df70cb833772b41c10b6e14c
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-05-22 18:47:48 -0400

    fixed password environment var call on server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let MySQLStore = require("express-mysql-session")(session)
 let options = {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
-    password: process.env.PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT
 }
