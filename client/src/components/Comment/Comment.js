d4bc57932820fc20df24313dacba6765ced6710f
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-07-02 09:52:26 -0400

    now have the ability to hide/show comment  threads

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -22,6 +22,7 @@ const Comment = ({ comment }) => {
   const [toggleCommentReply, setToggleCommentReply] = useState(false)
   const [isEditing, setIsEditing] = useState(false);
   const [editCommentBody, setEditCommentBody] = useState();
+  const [threadToggle, setThreadToggle] = useState(true);
 
 
   let createdAt = getElapsedTime(comment.created_at)
@@ -33,7 +34,7 @@ const Comment = ({ comment }) => {
 
   useEffect(() => {
     setChildComments(getChildComments(comment))
-  }, [comments])
+  }, [])
 
 
   const handleEditCommentFormSubmit = (e, id) => {
@@ -52,103 +53,97 @@ const Comment = ({ comment }) => {
 
   return (
     <div className="comment-thread">
-      {/* { childComments && 
-        <div className="bar"></div>
-      } */}
-      <>
-        <div className={`comment ${darkMode && 'dark'}`}>
-          <div className="comment-main-section">
-              <p className="comment-metadata">
-              <span className="user">{comment.username}</span>
-              {createdAt}
-            </p>
 
+      <div className={`comment ${darkMode && 'dark'}`}>
+        <div className="comment-main-section">
+            <p className="comment-metadata">
+            <span className="user">{comment.username}</span>
+            {createdAt}
+          </p>
 
+          <div className="comment-body">
+            { user && user.id === comment.author_id && isEditing ?
+              <form className="edit-comment-form" onSubmit={(e) => {
+                handleEditCommentFormSubmit(e, comment.id)}
 
-            <div className="comment-body">
-              { user && user.id === comment.author_id && isEditing ?
-                <form className="edit-comment-form" onSubmit={(e) => {
-                  handleEditCommentFormSubmit(e, comment.id)}
-
-                }>
-                  <textarea value={editCommentBody} onChange={(e) => setEditCommentBody(e.target.value)} />
-                  <div className="save-and-cancel">
-                    <button type="submit">Save</button>
-                    <button onClick={() => setIsEditing(!isEditing)}>Cancel</button>
-                  </div>
-                </form>
-                :
-                <p className="comment-body-text">{comment.body}</p>
-              }
-            </div>
-
-
-
-            { user &&
-              <>
-              { !toggleCommentReply &&
-                <button className="reply-button" onClick={() => setToggleCommentReply(!toggleCommentReply)}><CommentIcon className="comment-icon"/> Reply</button>
-              }
-                
-                { toggleCommentReply &&
-                    <CommentForm
-                      post={post}
-                      parentComment={comment.id}
-                      setToggleCommentReply={setToggleCommentReply}
-                      alwaysOpen={false}
-                    />
-                }
-              </>
+              }>
+                <textarea value={editCommentBody} onChange={(e) => setEditCommentBody(e.target.value)} />
+                <div className="save-and-cancel">
+                  <button type="submit">Save</button>
+                  <button onClick={() => setIsEditing(!isEditing)}>Cancel</button>
+                </div>
+              </form>
+              :
+              <p className="comment-body-text">{comment.body}</p>
             }
           </div>
-          
-
-
-          {user && user.id === comment.author_id  && (
-            <div className="commented-user-accessable-options">
-              { !isEditing &&
-                <button
-                  onClick={() => setIsEditing(!isEditing)}
-                >
-                <EditIcon
-                  className="edit-icon"
-                />
-              </button>
-              }
-
-              <button
-                onClick={() => dispatch(deleteComment(comment.id))}
-              >
-                <DeleteIcon
-                  className="delete-icon"
-                /> 
-              </button>
-            </div>
-          )}
 
 
 
-        </div>      
-        { childComments &&
-        <div className="nested-comment">
-          
-          {
-            childComments.map((childComment, key) => 
+          { user &&
             <>
-              <div className="bar"></div>
-              <Comment key={key} comment={childComment}/>
+            { !toggleCommentReply &&
+              <button className="reply-button" onClick={() => setToggleCommentReply(!toggleCommentReply)}><CommentIcon className="comment-icon"/> Reply</button>
+            }
+              
+              { toggleCommentReply &&
+                  <CommentForm
+                    post={post}
+                    parentComment={comment.id}
+                    setToggleCommentReply={setToggleCommentReply}
+                    alwaysOpen={false}
+                  />
+              }
             </>
+          }
+        </div>
+        
 
+
+        {user && user.id === comment.author_id  && (
+          <div className="commented-user-accessable-options">
+            { !isEditing &&
+              <button
+                onClick={() => setIsEditing(!isEditing)}
+              >
+              <EditIcon
+                className="edit-icon"
+              />
+            </button>
+            }
+
+            <button
+              onClick={() => dispatch(deleteComment(comment.id))}
+            >
+              <DeleteIcon
+                className="delete-icon"
+              /> 
+            </button>
+          </div>
+        )}
+      </div>      
+      
+      { comments &&
+      <>            
+      <div className="nested-comment">
+              <p onClick={() => setThreadToggle(!threadToggle)} className='replies-toggle'>{`${threadToggle ? 'Hide ' : 'Show '}`}replies</p>
+          {
+            comments.map((eachComment, key) => eachComment.parent_comment === comment.id &&         
+
+              threadToggle &&
+                <div className="bar-and-comment">
+                  <div onClick={() => setThreadToggle(!threadToggle)} className="bar"></div>
+                  <Comment key={key} comment={eachComment}/>
+                </div>
+                
+              
+             
             )
           }
-        </div>  
-        }
+        </div> 
       </>
-
-      
-
+      }
     </div>
- 
   )
 }
 
