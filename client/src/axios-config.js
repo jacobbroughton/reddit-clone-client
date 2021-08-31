79da6d911d09763d66554f3155ab51f7b3e7644c
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-08-30 21:42:53 -0400

    added production server route to axios config

--- client/src/axios-config.js
+++ client/src/axios-config.js
@@ -2,10 +2,10 @@ import axios from "axios"
 // import store from "./store/store"
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
+    baseURL: process.env.NODE_ENV === "development" ? 'http://localhost:5000/' : 'https://reddit-clone-jb.herokuapp.com/',
     withCredentials: true
 })
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? 'http://localhost:5000/' : 'https://reddit-clone-jb.herokuapp.com/'
 
 export default instance
