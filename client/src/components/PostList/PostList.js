b791c48420f9ccf3500f018f0eb25d0af8bbc8ab
Jacob Broughton <42415136+jlbroughton88@users.noreply.github.com>
2021-09-25 21:04:03 -0400

    added component for prompting users to create a new post if a subreddit doesn't have any

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -3,17 +3,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { withRouter, useParams } from "react-router-dom";
 import React, { useEffect } from "react";
 import { getPosts } from "../../actions/postListActions";
-import { search } from "../../actions/searchActions"
+import { search } from "../../actions/searchActions";
 import { setCurrentSubreddit } from "../../actions/subredditActions";
-import { useQuery } from "../useQuery"
+import { useQuery } from "../useQuery";
 import Post from "../Post/Post";
 import SubredditsSelect from "../SubredditsSelect/SubredditsSelect";
-import CurrentSubredditBanner from "../CurrentSubredditBanner/CurrentSubredditBanner"
+import CurrentSubredditBanner from "../CurrentSubredditBanner/CurrentSubredditBanner";
 import Loading from "../Loading/Loading";
+import NoPostsPrompt from "../NoPostsPrompt/NoPostsPrompt";
 
 const PostList = () => {
   const dispatch = useDispatch();
-  const query = useQuery()
+  const query = useQuery();
 
   const user = useSelector((state) => state.auth.user);
   const darkMode = useSelector((state) => state.darkMode);
@@ -24,41 +25,37 @@ const PostList = () => {
   let searchQueryFromURL = query.get("q");
 
   useEffect(() => {
+    dispatch(setCurrentSubreddit(name ? name : null));
 
-    dispatch(setCurrentSubreddit(name ? name : null))
-
-    if(searchQueryFromURL) {
-      console.log("Search: " + searchQueryFromURL)
-      dispatch(search(user?.id, name, searchQueryFromURL)) 
+    if (searchQueryFromURL) {
+      console.log("Search: " + searchQueryFromURL);
+      dispatch(search(user?.id, name, searchQueryFromURL));
     } else {
       dispatch(getPosts(user ? user.id : null, name ? name : null));
     }
-  }, [searchQueryFromURL, name, user])
+  }, [searchQueryFromURL, name, user]);
 
   return (
-    <div className={`post-list-main ${darkMode ? 'dark' : ''}`}>
-      <SubredditsSelect/>
-      <CurrentSubredditBanner name={name} user={user}/>
-      {loading ?
-        <Loading/>
-        :
+    <div className={`post-list-main ${darkMode ? "dark" : ""}`}>
+      <SubredditsSelect />
+      <CurrentSubredditBanner name={name} user={user} />
+      {loading ? (
+        <Loading />
+      ) : (
         <>
           <div className="post-list-container">
             <div className="post-list">
-                <>
-                  {posts.map((post, key) => (
-                    <Post
-                      post={post}
-                      key={key}
-                    />
-                  ))}
-                </>
+              <>
+                {posts.length > 0 ? 
+                  posts.map((post, key) => <Post post={post} key={key} />)
+                  : 
+                  <NoPostsPrompt user={user} darkMode={darkMode}/>
+                }
+              </>
             </div>
           </div>
         </>
-
-      }
-      
+      )}
     </div>
   );
 };
