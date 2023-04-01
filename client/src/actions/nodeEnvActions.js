a1cfe11823f60ed59ac5dbe34e63cac3bffd84da
Jacob Broughton <jlbroughton88@gmail.com>
2023-04-01 13:44:30 -0400

    heroku test

--- client/src/actions/nodeEnvActions.js
+++ client/src/actions/nodeEnvActions.js
@@ -2,7 +2,7 @@ export const getApiUrl = () => {
   let API_URL
 
   if (import.meta.env.PROD) {
-    API_URL = "https://reddit-clone-jb.herokuapp.com"
+    API_URL = "https://reddit-clone-api.onrender.com/"
   } else {
     API_URL = "http://localhost:5000"
   }
